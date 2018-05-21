a6355da251ae870cd4abcc635bcfcd4e2aa80829
KarlJohnsonIntrepid <karljohnson@live.co.uk>
2018-05-22 06:56:33 +1000

    flowcontrol

---
+++ basics/boolean.js
@@ -0,0 +1,24 @@
+// === equality
+// !== not equal
+// < less than
+// > greater than
+// >= greater than or equal to
+//<= equal to
+
+let temp = 31;
+let isFreezing = temp === 31;
+//console.log(isFreezing);
+
+//Challenge
+//Create Age variable yoir age
+//Caclulate is child < 7
+//I sseniot 65>=
+//Print child print is sneiot
+let age = 32;
+let output = 'adult';
+if(age <7){
+   output = 'child';
+} else if( age >= 65){
+   output= 'adult';
+}
+console.log(output);
